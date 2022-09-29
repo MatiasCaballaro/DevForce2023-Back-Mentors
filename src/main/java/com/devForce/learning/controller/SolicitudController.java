a87e2faa12b8f02fa7755c0e3780948dfb1cdbe2
Matias Caballaro <88558170+MatiasCaballaro@users.noreply.github.com>
2022-09-29 00:11:51 -0300

    * Prueba para solucionar el CORS problem.

--- src/main/java/com/devForce/learning/controller/SolicitudController.java
+++ src/main/java/com/devForce/learning/controller/SolicitudController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class SolicitudController {
